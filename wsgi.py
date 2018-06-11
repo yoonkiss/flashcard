0dc580befb8fc21fae2fbe642dcf61ee1d252eed
yoonkiss <park.yoonki@gmail.com>
2018-06-11 23:04:15 +0900

    draft

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,6 @@
-from flask import Flask
 from flash_cards import app
 
-application = Flask(__name__)
-
-@application.route("/")
-def hello():
-    return "Hello World!"
-
 if __name__ == "__main__":
-    application.run()
+    app.run()
 
 
