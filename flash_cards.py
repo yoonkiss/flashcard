95fca3c1763cd6e29063affa6f88d35511740d34
yoonkiss <park.yoonki@gmail.com>
2018-06-19 18:49:22 +0900

    draft

--- flash_cards.py
+++ flash_cards.py
@@ -49,10 +49,10 @@ def close_db(error):
 
 # Uncomment and use this to initialize database, then comment it
 #   You can rerun it to pave the database and start over
-@application.route('/initdb')
-def initdb():
-    init_db()
-    return 'Initialized the database.'
+#@application.route('/initdb')
+#def initdb():
+#    init_db()
+#    return 'Initialized the database.'
 
 
 @application.route('/')
